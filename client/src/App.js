e9cb02e7ec66c1cf190e20182ab8c5f1eca8cf0f
Harlan Evans <harlanevans5@gmail.com>
2019-01-02 17:06:11 -0700

    added login

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import NoMatch from './components/NoMatch';
+import Login from './components/Login';
 
 import { Container, } from "semantic-ui-react";
 import { Switch, Route, } from 'react-router-dom';
@@ -13,6 +14,7 @@ const App = () => (
     <Container>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path='/login' component={Login} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
